a39557946423f37c77b1011bbefd40bdb9477ee2
jkhopkins39 <jeremyyhop@gmail.com>
2025-02-22 13:10:49 -0500

    updated nav

--- nav.js
+++ nav.js
@@ -13,6 +13,7 @@ export function createNavBar() {
                     <li><a href="index.html" class="nav-link ${window.location.pathname.endsWith('index.html') || window.location.pathname === '/' ? 'active' : ''}">Home</a></li>
                     <li><a href="leaderboard.html" class="nav-link ${window.location.pathname.endsWith('leaderboard.html') ? 'active' : ''}">Leaderboard</a></li>
                     <li><a href="rewards.html" class="nav-link ${window.location.pathname.endsWith('rewards.html') ? 'active' : ''}">Rewards</a></li>
+                    <li><a href="events.html" class="nav-link ${window.location.pathname.endsWith('events.html') ? 'active' : ''}">Events</a></li>
                 </ul>
             </div>
 
